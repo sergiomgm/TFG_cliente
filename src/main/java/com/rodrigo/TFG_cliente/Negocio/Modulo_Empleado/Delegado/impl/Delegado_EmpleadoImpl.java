6ac5c5fa6e819ea4165715e2ff4f269daf5e527f
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-10-29 22:42:02 +0100

    WS-Security funcionando en Empleados

--- src/main/java/com/rodrigo/TFG_cliente/Negocio/Modulo_Empleado/Delegado/impl/Delegado_EmpleadoImpl.java
+++ src/main/java/com/rodrigo/TFG_cliente/Negocio/Modulo_Empleado/Delegado/impl/Delegado_EmpleadoImpl.java
@@ -1,5 +1,6 @@
 package com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Delegado.impl;
 
+import com.eduardosergio.TFG_cliente.seguridad.ClientPasswordCallback;
 import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Delegado.Delegado_Empleado;
 import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Entidad.Transfers.TEmpleado;
 import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Entidad.Transfers.TEmpleadoCompleto;
@@ -8,14 +9,27 @@ import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Excepciones.EmpleadoField
 import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Excepciones.EmpleadoYaExisteExcepcion;
 import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Serv_aplicacion.IBroker_SA_Empleado;
 import com.rodrigo.TFG_cliente.Presentacion.Proxy.Excepciones.ProxyException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -48,6 +62,7 @@ public class Delegado_EmpleadoImpl extends Delegado_Empleado {
 
 
     public Delegado_EmpleadoImpl() throws ProxyException {
+    	/*
         log.info("Creando Delegado_EmpleadoImpl");
 
         log.info("Creando Qname del servicio");
@@ -83,8 +98,62 @@ public class Delegado_EmpleadoImpl extends Delegado_Empleado {
         req_ctx2.put(BindingProvider.PASSWORD_PROPERTY, "contra");
 
         req_ctx2.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, URL_SERVICE);
+        */	
+	    
+    	
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:cxf.xml");
+        
+        portEmpleados = (IBroker_SA_Empleado) context.getBean("SA_Empleado");
+        
+        ((BindingProvider) portEmpleados).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "usuario");
+        ((BindingProvider) portEmpleados).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "contra");
+        
+        ((BindingProvider) portEmpleados).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, URL_SERVICE);
 
+/*
+    	 JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+	        
+	        factory.setAddress(URL_SERVICE);
+	        
+	        factory.setServiceClass(IBroker_SA_Empleado.class);
+	        
+	        Object client = factory.create();
+	        
+	        LoggingOutInterceptor loggingOutInterceptor = new LoggingOutInterceptor();
+	        loggingOutInterceptor.setPrettyLogging(true);
+	        ClientProxy.getClient(client).getOutInterceptors().add(loggingOutInterceptor);
+	        
+	        LoggingInInterceptor loggingInInterceptor = new LoggingInInterceptor();
+	        loggingInInterceptor.setPrettyLogging(true);
+	        ClientProxy.getClient(client).getInInterceptors().add(loggingInInterceptor);
+	        
+	        //Configuracion encriptacion de salida WS-Security
+	        Map<String, Object> props = new HashMap<String, Object>();
+	        props.put(WSHandlerConstants.USER, "testkey");
+	        props.put(WSHandlerConstants.ACTION, WSHandlerConstants.ENCRYPT);
+	        props.put(WSHandlerConstants.PASSWORD_TYPE, "PasswordText");
+	        props.put(WSHandlerConstants.ENC_PROP_FILE, "clientkeystore.properties");
+	        props.put(WSHandlerConstants.ENCRYPTION_PARTS, "{Content}{http://schemas.xmlsoap.org/soap/envelope/}Body");
+	        props.put(WSHandlerConstants.PW_CALLBACK_CLASS, ClientPasswordCallback.class.getName());
 
+	        WSS4JOutInterceptor wss4jOut = new WSS4JOutInterceptor(props);
+	        ClientProxy.getClient(client).getOutInterceptors().add(wss4jOut);
+	        
+	        //Configuracion encriptacion de entrada WS-Security
+	        Map<String, Object> inProps = new HashMap<String, Object>();
+	        inProps.put(WSHandlerConstants.USER, "clienttestkey");
+	        inProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.ENCRYPT);
+	        inProps.put(WSHandlerConstants.PASSWORD_TYPE, "PasswordText");
+	        inProps.put(WSHandlerConstants.SIG_PROP_FILE, "clientkeystore.properties"); 
+	        inProps.put(WSHandlerConstants.DEC_PROP_FILE, "clientkeystore.properties");
+	        inProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, ClientPasswordCallback.class.getName());
+	        
+	        
+	        WSS4JInInterceptor wss4jIn= new WSS4JInInterceptor(inProps);
+	        ClientProxy.getClient(client).getInInterceptors().add(wss4jIn);
+	   		
+	        portEmpleados = (IBroker_SA_Empleado) client;
+	        */
 
         log.info("DelegadoDelNegocio creado");
     }
--- src/main/java/com/rodrigo/TFG_cliente/Negocio/Modulo_Empleado/Serv_aplicacion/IBroker_SA_Empleado.java
+++ src/main/java/com/rodrigo/TFG_cliente/Negocio/Modulo_Empleado/Serv_aplicacion/IBroker_SA_Empleado.java
@@ -18,7 +18,7 @@ import java.util.List;
  * TFG - Atravesando las Capas de una Aplicación Empresarial: Demostrador Tecnológico J2EE
  */
 @WebService(
-        targetNamespace = "http://impl.Serv_aplicacion.Modulo_Empleado.Negocio.TFG_server.rodrigo.com/",
+        targetNamespace = "http://Serv_aplicacion.Modulo_Empleado.Negocio.TFG_server.rodrigo.com/",
         name = "Broker_SA_EmpleadoImpl")
 public interface IBroker_SA_Empleado {
 
