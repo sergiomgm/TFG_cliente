e575be38f6f567d8280df2d65121b7cd6fec2fb7
Eduardo Romero Palencia <eduarrom@ucm.es>
2019-03-21 10:48:11 +0100

    Error Policy Verificator

--- src/main/java/com/eduardosergio/TFG_cliente/negocio/helloWorld/Serv_aplicacion/HelloWorld.java
+++ src/main/java/com/eduardosergio/TFG_cliente/negocio/helloWorld/Serv_aplicacion/HelloWorld.java
@@ -1,12 +1,11 @@
 package com.eduardosergio.TFG_cliente.negocio.helloWorld.Serv_aplicacion;
 
 import javax.jws.WebMethod;
-import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@WebService(name = "HelloWorld", targetNamespace = "http://helloWorld.negocio.TFG_server.eduardosergio.com/")
+@WebService(name = "HelloWorldImplService", targetNamespace = "http://helloWorld.negocio.TFG_server.eduardosergio.com/")
 public interface HelloWorld {
-
-	@WebMethod(operationName = "salute", action = "urn:Salute")
+	
+	@WebMethod
 	public String salute();
 }
